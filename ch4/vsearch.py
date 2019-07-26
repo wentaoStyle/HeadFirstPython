10c8759e19abb50d1f11cba9d5096a3ffadc5e24
wentaoStyle <49229942+wentaoStyle@users.noreply.github.com>
2019-07-26 13:58:40 +0800

    Update vsearch.py

--- ch4/vsearch.py
+++ ch4/vsearch.py
@@ -1,8 +1,6 @@
 
 def search4vowels(phrase:str) ->set:
-    """Returns the set of vowels found in 'Phrase'."""
     return set('aeiou').intersection(set(phrase))
 
 def search4letter(phrase:str,letters:str = 'aeiou') ->set:
-    """Returns the set of 'letters' found in 'phrase'."""
     return set(letetrs).intersection(phrase)
